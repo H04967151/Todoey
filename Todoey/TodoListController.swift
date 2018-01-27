e05a94c70ed5e1f60b7034528fe104aec5d47d96
neil williams <f_u_b_a_r2003@hotmail.com>
2018-01-27 16:16:13 +0000

    Initial tableview creation completed

---
+++ Todoey/TodoListController.swift
@@ -0,0 +1,53 @@
+//
+//  ViewController.swift
+//  Todoey
+//
+//  Created by Neil on 27/01/2018.
+//  Copyright © 2018 Neil. All rights reserved.
+//
+
+import UIKit
+
+class TodoListController: UITableViewController {
+    
+    let itemArray = ["Feed dog", "Buy dog food", "Make dinner"]
+    let cellID = "TodoItemCell"
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+}
+
+extension TodoListController{
+    
+    //MARK - Tableview Datasource
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellID, for: indexPath)
+        cell.textLabel?.text = itemArray[indexPath.row]
+        return cell
+    }
+    
+    //MARK - Tableview Delegates
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let cell = tableView.cellForRow(at: indexPath)
+        if cell?.accessoryType == .checkmark {
+            cell?.accessoryType = .none
+        }else{
+            cell?.accessoryType = .checkmark
+        }
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+    
+    
+}
